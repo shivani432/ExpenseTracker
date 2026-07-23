cd4e3da64bde9d605811e8f7326498ca9c378e4c
shivani kantak <shivanikantak10@gmail.com>
2026-07-23 20:26:49 +0530

    Complete Version 12 - Spring Boot CRUD APIs

--- backend/expense-tracker-backend/src/main/java/com/shivani/expensetracker/controller/ExpenseController.java
+++ backend/expense-tracker-backend/src/main/java/com/shivani/expensetracker/controller/ExpenseController.java
@@ -1,20 +1,49 @@
 package com.shivani.expensetracker.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shivani.expensetracker.entity.Expense;
 import com.shivani.expensetracker.service.ExpenseService;
 
 @RestController
 public class ExpenseController {
 
-    @Autowired
-    private ExpenseService expenseService;
+        @Autowired
+        private ExpenseService expenseService;
 
-    @GetMapping("/")
-    public String home() {
-        return expenseService.getMessage();
+        @GetMapping("/")
+        public String home() {
+            return expenseService.getMessage();
+    }
+        @PostMapping("/expenses")
+        public Expense saveExpense(@RequestBody Expense expense) {
+        return expenseService.saveExpense(expense);
+    }
+        @GetMapping("/expenses")
+        public List<Expense> getAllExpenses() {
+        return expenseService.getAllExpenses();
+    }
+        @GetMapping("/expenses/{id}")
+        public Expense getExpenseById(@PathVariable Long id) {
+        return expenseService.getExpenseById(id);
+    }
+        @PutMapping("/expenses/{id}")
+        public Expense updateExpense(@PathVariable Long id,
+                                @RequestBody Expense updatedExpense) {
+        return expenseService.updateExpense(id, updatedExpense);
+    }
+        @DeleteMapping("/expenses/{id}")
+        public void deleteExpense(@PathVariable Long id) {
+        expenseService.deleteExpense(id);
     }
 
 }
--- backend/expense-tracker-backend/src/main/java/com/shivani/expensetracker/service/ExpenseService.java
+++ backend/expense-tracker-backend/src/main/java/com/shivani/expensetracker/service/ExpenseService.java
@@ -1,8 +1,11 @@
 package com.shivani.expensetracker.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.shivani.expensetracker.entity.Expense;
 import com.shivani.expensetracker.repository.ExpenseRepository;
 
 @Service
@@ -13,6 +16,33 @@ public class ExpenseService {
 
       public String getMessage() {
         return "Expense Service is Working!";
+  }
+    public Expense saveExpense(Expense expense) {
+    return expenseRepository.save(expense);
+  }
+    public List<Expense> getAllExpenses() {
+    return expenseRepository.findAll();
+  }
+    public Expense getExpenseById(Long id) {
+    return expenseRepository.findById(id).orElse(null);
+  }
+    public Expense updateExpense(Long id, Expense updatedExpense) {
+
+    Expense expense = expenseRepository.findById(id).orElse(null);
+
+    if (expense != null) {
+        expense.setTitle(updatedExpense.getTitle());
+        expense.setAmount(updatedExpense.getAmount());
+        expense.setCategory(updatedExpense.getCategory());
+        expense.setDate(updatedExpense.getDate());
+
+        return expenseRepository.save(expense);
     }
+
+    return null;
+  }
+    public void deleteExpense(Long id) {
+    expenseRepository.deleteById(id);
+  }
 
 }
